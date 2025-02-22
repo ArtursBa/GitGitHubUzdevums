584376ce8c9c8aa0508684f39504fc55e23ed7c8
ArtursBa <artursb15@gmail.com>
2025-02-22 14:36:41 +0200

    Pievienota Arturs klase un atjauninaats main.cpp

---
+++ Arturs.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Name.h"
+#include "Arturs.h"
+
+using namespace std;
+
+Arturs::Arturs(string n)
+{
+	name=n;
+}
+
+void Arturs::setName(string n)
+{
+	name=n;
+}
+
+void Arturs::Print() const
+{
+	cout << name << endl;
+}
